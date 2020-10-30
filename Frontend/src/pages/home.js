8bb9dea20564ac94d9d18b38abf988a5d2d99ee3
Mateo-714 <mateo.arias714@gmail.com>
2020-10-29 20:21:11 -0500

    si

--- Frontend/src/pages/home.js
+++ Frontend/src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header'
 import Hom from '../components/Home'
-
+import '../styles/styles.css'
 export default function home(){
     
     return(
