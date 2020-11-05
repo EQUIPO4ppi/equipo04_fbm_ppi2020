7293ab249fdd97d45563d5e66028e9fce969988b
Mateo-714 <mateo.arias714@gmail.com>
2020-11-05 02:02:48 -0500

    Pantallas de menú, notificaciones y calendario

--- Frontend/src/routes/App.js
+++ Frontend/src/routes/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import home from '../pages/home';
 import Card from '../pages/Card'
+import Notificacion from '../pages/notificacion'
+import Calendario from '../pages/Calendario'
+
 
 const App = () => {
     return (
@@ -10,7 +13,8 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component = {home} />
                 <Route path="/Card" component = {Card} />
-
+                <Route path="/Notificaciones" component = {Notificacion} />
+                <Route path="/Calendario" component = {Calendario} />
                
             </Switch>
 
